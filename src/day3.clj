4d4a37d4f4f7c98099272504d23ba1f2dd4acd8f
Chris Dombroski <cdombroski@gmail.com>
2022-12-09 12:22:52 -0500

    Some refactoring, correct answer despite same answer

--- src/day3.clj
+++ src/day3.clj
@@ -2,34 +2,39 @@
   (:require [clojure.java.io :as io]
             [clojure.set :as set]))
 
+(defn priority [char]
+  (->> char (map int)
+       (map #(if (>= % 97)
+               (- % 96)
+               (- % 38)))))
+
+(defn intersection [llst]
+  (->> llst
+       (map #(map set %))
+       (map #(apply set/intersection %))
+       (map first)))
+
+(defn read-char-lines [input]
+  (->> input
+       line-seq
+       (map seq)))
+
 (defn problem1 []
   (with-open [input (io/reader "resource/day3.txt")]
     (->> input
-         line-seq
-         (map seq)
+         read-char-lines
          (map #(split-at (/ (count %) 2) %))
-         (map #(map set %))
-         (map #(apply set/intersection %))
-         (map first)
-         (map int)
-         (map #(if (>= % 97)
-                 (- % 96)
-                 (- % 38)))
+         intersection
+         priority
          (reduce +))))
 
 (defn problem2 []
   (with-open [input (io/reader "resource/day3.txt")]
     (->> input
-         line-seq
-         (map seq)
-         (map set)
+         read-char-lines
          (partition 3)
-         (map #(apply set/intersection %))
-         (map first)
-         (map int)
-         (map #(if (>= % 97)
-                 (- % 96)
-                 (- % 38)))
+         intersection
+         priority
          (reduce +))))
 
 (println (problem1))
